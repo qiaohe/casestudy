454b029e5cf0bb1a196973a4a9c213c535b66d11
tusc_heqiao@163.com <heqiao75518?>
2014-08-29 15:45:28 +0800

    Initial commit

--- src/main/java/cn/mobiledaily/huangang/CsvReader.java
+++ src/main/java/cn/mobiledaily/huangang/CsvReader.java
@@ -21,10 +21,10 @@ public class CsvReader {
     private static final String INSERT_SQL = "INSERT INTO TradeInfo(shipper,shipperaddr1,shipperaddr2,shipperaddr3," +
             "consignee,consigneeaddr1,consigneeaddr2,consigneeaddr3,notifypartyname,notifypartyaddr1,notifypartyaddr2" +
             ",notifypartyaddr3,[weight],weightunit,arrivaldate,pieces,piecesunit,measure,measureunit,origin,vessel," +
-            "goodsdesc,carrier,carriersimple,voyage) " +
+            "goodsdesc,carrier,carriersimple,voyage,portofembarkation,portofarrival,blno,usdest,transportmode,precarrier,fordest) " +
             "values(''{0}'',''{1}'',''{2}'',''{3}'',''{4}'',''{5}'',''{6}'',''{7}'',''{8}'',''{9}'',''{10}'',''{11}''," +
             "''{12}'',''{13}'',''{14}'',''{15}'',''{16}'',''{17}'',''{18}'',''{19}'',''{20}'',''{21}'',''{22}''," +
-            " ''{23}'',''{24}'')";
+            " ''{23}'',''{24}'',''{25}'',''{26}'',''{27}'',''{28}'',''{29}'',''{30}'', ''{31}'')";
     private static final CSVFormat CSV_FORMAT = CSVFormat.newFormat('|').withSkipHeaderRecord(true).withHeader("Bill_Of_Lading_Nbr", "Arrival_Date", "House Master", "Master B L", "Carrier_Code", "Master_Carrier", "Vessel_Name", "Vessel_Code", "Voyage_Number", "Mode_Transport", "Foreign_Port_Lading", "Place_Receipt", "DP_Of_Unlading", "Inbond_Entry_Type", "US_Dist_Port", "Foreign_Distribution_Port", "FROB", "Weight", "Weight_Unit", "Manifest_Qty", "Manifest_Units", "Measurement", "Measurement_Unit", "Estimated Arrival", "Actual Arrival", "Piece_Count", "Cargo_Description", "Shipper_Name", "Shipper_Addr_1", "Shipper_Addr_2", "Shipper_Addr_3", "Shipper_Addr_4", "Consignee_Name", "Consignee_Addr_1", "Consignee_Addr_2", "Consignee_Addr_3", "Consignee_Addr_4", "Notify_Name", "Notify_Addr_1", "Notify_Addr_2", "Notify_Addr_3", "Notify_Addr_4", "Notify_2_Name", "Notify_2_Addr_1", "Notify_2_Addr_2", "Notify_2_Addr_3", "Notify_2_Addr_4", "Container_Number", "Container_Marks", "Container_Length", "Container_Height", "Container_Width")
             .withSkipHeaderRecord(true).withRecordSeparator('|').withIgnoreEmptyLines(false);
 
@@ -62,7 +62,9 @@ public class CsvReader {
                     , replace(record.get("Notify_Addr_1")), replace(record.get("Notify_Addr_2")), replace(record.get("Notify_Addr_3")), replace(record.get("Weight"))
                     , replace(record.get("Weight_Unit")), replace(record.get("Arrival_Date")), replace(record.get("Manifest_Qty")), replace(record.get("Manifest_Units"))
                     , replace(record.get("Measurement")), replace(record.get("Measurement_Unit")), replace(record.get("Place_Receipt")), replace(record.get("Vessel_Name")),
-                    replace(record.get("Cargo_Description")), replace(record.get("Master_Carrier")), replace(record.get("Carrier_Code")), replace(record.get("Voyage_Number")));
+                    replace(record.get("Cargo_Description")), replace(record.get("Master_Carrier")), replace(record.get("Carrier_Code")), replace(record.get("Voyage_Number")),
+                    replace(record.get("Foreign_Port_Lading")), replace(record.get("DP_Of_Unlading")), replace(record.get("Master B L")),replace(record.get("DP_Of_Unlading"))
+            ,replace(record.get("Mode_Transport")), replace(record.get("Place_Receipt")), replace(record.get("Place_Receipt")));
             try {
                 statement.addBatch(query);
             } catch (Exception ex) {
